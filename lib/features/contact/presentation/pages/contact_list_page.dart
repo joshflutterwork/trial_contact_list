33447f263d50a0b925ef9b24f81ebc1894e6388c
joshflutterwork <joshuaevanmannaim@gmail.com>
2024-10-20 16:44:39 +0700

    chore : add TDD folder structure
    adding page

---
+++ lib/features/contact/presentation/pages/contact_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactListPage extends StatefulWidget {
+  const ContactListPage({super.key});
+
+  @override
+  State<ContactListPage> createState() => _ContactListPageState();
+}
+
+class _ContactListPageState extends State<ContactListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
